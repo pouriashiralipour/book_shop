7c36ec50cc63aa98e73a1e80b5af5cd4e3a0af98
Pouria <pouriashiralipour@gmail.com>
2023-07-31 01:50:35 +0330

    add custom user change form class

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,15 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('age',)
+
+
+class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ('age',)
