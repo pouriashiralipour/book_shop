7be03682313718c1ac8ace2f58718f88ddcbcd97
Pouria <pouriashiralipour@gmail.com>
2023-07-31 13:19:09 +0330

    create view and template for listview

--- books/views.py
+++ books/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Book
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/list_view.html'
