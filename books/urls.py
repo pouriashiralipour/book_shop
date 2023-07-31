54c059a310860caec9652cd434590c1c9d2ee184
Pouria <pouriashiralipour@gmail.com>
2023-07-31 22:45:30 +0330

    add front basic code for update view

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import BookListView, BookDetailsView, BookCreateView
+from .views import BookListView, BookDetailsView, BookCreateView, BookUpdateView, BookDeleteView
 
 app_name = 'book'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', BookListView.as_view(), name='list_view'),
     path('<int:pk>/', BookDetailsView.as_view(), name='details_view'),
     path('new/', BookCreateView.as_view(), name='create_view'),
+    path('<int:pk>/update/', BookUpdateView.as_view(), name='update_view')
 ]
