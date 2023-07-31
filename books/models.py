26fb1914e3c93ce0a40d494e2dcc8d2d151cba88
Pouria <pouriashiralipour@gmail.com>
2023-07-31 13:13:46 +0330

    create book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    content = models.TextField()
+    price = models.PositiveBigIntegerField()
+    datetime_created = models.DateTimeField(default=timezone.now)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'book'
+        verbose_name_plural = 'books'
+        ordering = ['-datetime_created']
+
+    def __str__(self):
+        return self.title
