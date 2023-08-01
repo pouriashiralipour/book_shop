27eecc3af8bde6744663ecb4740aeb3a631e5790
Pouria <pouriashiralipour@gmail.com>
2023-08-01 17:15:01 +0330

    add static file to root of project and add it to settings.py

--- config/settings.py
+++ config/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+# static files config
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
