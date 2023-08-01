f4fc44accbaf99b021940fb2f5a6df77c9d1f3f2
Pouria <pouriashiralipour@gmail.com>
2023-08-01 23:14:51 +0330

    add list view to home page

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from books.models import Book
 
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
-
+class HomePageView(ListView):
+    model = Book
+    template_name = 'books/list_view.html'
+    context_object_name = 'books'
