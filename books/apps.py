0a981e3f96ce0c0063f7414186a06518d41e4c78
Pouria <pouriashiralipour@gmail.com>
2023-08-02 16:37:08 +0330

    translate models book

--- books/apps.py
+++ books/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy, gettext as _
 
 
 class BooksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'books'
+    name = _('book')
--- books/models.py
+++ books/models.py
@@ -2,22 +2,23 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext, gettext_lazy as _
 
 
 class Book(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    translator = models.CharField(max_length=200, blank=True)
-    content = models.TextField()
-    price = models.PositiveBigIntegerField()
-    cover = models.ImageField(upload_to='covers/', blank=True)
-    datetime_created = models.DateTimeField(default=timezone.now)
-    datetime_modified = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('user'))
+    title = models.CharField(max_length=200, verbose_name=_('title'))
+    author = models.CharField(max_length=200, verbose_name=_('author'))
+    translator = models.CharField(max_length=200, blank=True, verbose_name=_('translator'))
+    content = models.TextField(verbose_name=_('content'))
+    price = models.PositiveBigIntegerField(verbose_name=_('price'))
+    cover = models.ImageField(upload_to='covers/', blank=True, verbose_name=_('cover'))
+    datetime_created = models.DateTimeField(default=timezone.now, verbose_name=_('datetime_created'))
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('datetime_modified'))
 
     class Meta:
-        verbose_name = 'book'
-        verbose_name_plural = 'books'
+        verbose_name = _('book')
+        verbose_name_plural = _('books')
         ordering = ['-datetime_created']
 
     def __str__(self):
@@ -28,12 +29,17 @@ class Book(models.Model):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
-    is_active = models.BooleanField(default=True)
-    recommend = models.BooleanField(default=True)
-    datetime_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_('user'))
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments', verbose_name=_('book'))
+    text = models.TextField(verbose_name=_('text'))
+    is_active = models.BooleanField(default=True, verbose_name=_('is_active'))
+    recommend = models.BooleanField(default=True, verbose_name=_('recommend'))
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_('datetime_created'))
+
+    class Meta:
+        verbose_name = _('comment')
+        verbose_name_plural = _('comments')
+        ordering = ['-datetime_created']
 
     def __str__(self):
         return f'{self.user}: {self.text}'
