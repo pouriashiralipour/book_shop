1dcf8586b1f132ba2b3b26935fa68ba9f98b25cd
Pouria <pouriashiralipour@gmail.com>
2023-08-02 16:44:23 +0330

    translate admin panel

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext, gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True, verbose_name=_('age'))
--- books/apps.py
+++ books/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
-from django.utils.translation import gettext_lazy, gettext as _
+from django.utils.translation import gettext, gettext_lazy as _
 
 
 class BooksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = _('book')
+    name = 'books'
+    verbose_name = _('books')
